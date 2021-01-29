0d312a58fe83750d6ede12ae7f39d3423cf58c53
NanoCode012 <kevinvong@rocketmail.com>
2021-01-29 13:55:20 +0700

    Fix gold not updating

--- bot.py
+++ bot.py
@@ -174,7 +174,7 @@ async def on_message(message):
                     status = 'won' if roll_val >= cutoff else 'lost'
                     out += f'You {status} {abs(diff)}.'
 
-                    db.document(f'fantasi/data/characters/{db_ref.id}').update({
+                    db_ref.update({
                         'gold': character.gold
                     })
 
