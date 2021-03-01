1488be601bd0b405de53a3831fe64efef9c55b5e
NanoCode012 <kevinvong@rocketmail.com>
2021-03-01 19:24:19 +0700

    Update run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -5,6 +5,6 @@ echo 'Removing any old containers if exist ..'
 sudo docker stop chanchan_discordbot || true && sudo docker rm chanchan_discordbot || true
 
 sudo docker build -t chanchan_discordbot .
-sudo docker run -d --env-file .env chanchan_discordbot
+sudo docker run -d --env-file .env --name chanchan_discordbot chanchan_discordbot
 
 echo 'Container has been built and is running ..'
