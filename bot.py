ef420c4dec743f7b30e1e4ddd85fb5196f2685d9
NanoCode012 <kevinvong@rocketmail.com>
2021-06-28 22:02:52 +0700

    Refactor rolls

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import random
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-from games import fantasi, rps
+from games import fantasi, rps, rolls
 
 # Use a service account
 cred = credentials.Certificate("firebase-adminsdk.json")
@@ -22,39 +22,6 @@ async def on_ready():
     await client.get_channel(int(os.getenv("DISCORD_CHANNEL_ID"))).send("I am backkkk!")
 
 
-def roll(message):
-    val = random.randint(1, 100)
-
-    roll_ref = db.collection("rolls").document()
-    roll_ref.set(
-        {
-            "channel_id": message.channel.id,
-            "author": {"id": message.author.id, "name": message.author.name,},
-            "value": val,
-        }
-    )
-    return val
-
-
-def roll_rank():
-    stream = (
-        db.collection("rolls")
-        .order_by("value", direction=firestore.Query.DESCENDING)
-        .limit(3)
-        .stream()
-    )
-
-    top = {}
-    for i, s in enumerate(stream):
-        d = s.to_dict()
-        name = d["author"]["name"]
-        val = d["value"]
-
-        top[i] = {"name": name, "value": val}
-
-    return top
-
-
 @client.event
 async def on_message(message):
     if message.author == client.user:
@@ -68,15 +35,8 @@ async def on_message(message):
     if message.content in messages:
         await message.channel.send(messages[message.content])
 
-    if message.content == "$roll":
-        await message.channel.send(f"{message.author.name} got {roll(message)}")
-
-    if message.content == "$roll-rank":
-        top = roll_rank()
-
-        for i in range(3):
-            nm, vl = top[i]["name"], top[i]["value"]
-            await message.channel.send(f"{i + 1}. {nm} {vl}")
+    if message.content.startswith == "$roll":
+        await rolls.handle(db=db, message=message)
 
     if message.content.startswith("$rps "):
         await rps.handle(db=db, message=message)
